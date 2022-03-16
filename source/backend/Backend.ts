1aab58f5a0f3d0c4eaedc632655dde97ce5c29f0
Jonas Heim <jonasbenedictheim@gmail.com>
2022-03-16 21:26:53 +0100

    Database Intigration

--- source/backend/Backend.ts
+++ source/backend/Backend.ts
@@ -1,5 +1,5 @@
+import { createComponent } from "@discordjs/builders"
 import { Collection, Guild, GuildMember } from "discord.js"
-
 export type Loot = {
   gulden: number
   items: string[]
@@ -10,13 +10,39 @@ export type LootItem = {
   item: string
 }
 
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database(':memory:');
+db.run("CREATE  TABLE  IF NOT EXIST schatzkammer(NR int(5) Primary Key Auto_Incroment, Item Varchar(20), gulden int(10), opfer Varchar(20)")
+
+var haltItem:Loot
+var bag:string[]
+
+
 export default class Backend {
   rob(user: GuildMember, loot: LootItem) {
     // mutate database
+    db.run("insert into schatzkammer (Item, Gulden, Opfer) Values  (?,?,?)", (loot.item, loot.gulden, user))
   }
 
   treasuryForGuild(guild: Guild): Collection<string, Loot> {
     // read database
-    return new Collection()
+    var rueckgabe = new Collection<string, Loot>()
+    db.each("select opfer, Sum(gulden) as guld, count(*) as anzahl from schatzkammer group by opfer", function(err: any, row: { opfer: string; guld: number; anzahl: any }){
+
+      while(bag.length != 0){
+        bag.pop
+      }
+  
+     db.each("select Item as i from schatzkammer where opfer = '(?)'", (row.opfer), function(err2: any, row2: { i: string }){
+       console.log(row2.i)
+        bag.push(row2.i)
+     } )
+     
+     haltItem.gulden = row.guld
+     haltItem.items = bag
+     rueckgabe.set(row.opfer, haltItem)
+    })
+  
+    return rueckgabe
   }
 }
